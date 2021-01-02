476b04b2f07a0796e87463c96040ba11f9127145
shifty <birukassefa123@gmail.com>
2021-01-02 16:25:12 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d65080089dadbaf0cb526b82f98a453'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63b8f39d-3032-480c-ab4e-4d87cb16d116'
+};

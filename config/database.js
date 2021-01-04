a3a9f7324c42a7b7f55a23f53c9825a0d1bd54dc
zshiftycrook <68913747+zshiftycrook@users.noreply.github.com>
2021-01-04 11:24:28 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'nonstopplccom_Rental'),
-        username: env('DATABASE_USERNAME', 'nonstopplccom_rental'),
+        username: env('DATABASE_USERNAME', 'nonstopplccom'),
         password: env('DATABASE_PASSWORD', 'password2020'),
         ssl: env.bool('DATABASE_SSL', false),
       },

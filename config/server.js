3ce8bdeb6fc77b91e4621d75201a54d65969945e
shifty <birukassefa123@gmail.com>
2021-02-09 04:03:39 +0300

    new

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1440),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5d65080089dadbaf0cb526b82f98a453'),
